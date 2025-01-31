dfae0eee7ac2d226f725792f06779a609d87a095
kiks <kiks>
2025-01-31 12:19:58 -0800

    a cluster for producer and consumer

--- server.js
+++ server.js
@@ -1,27 +1,44 @@
 // server.js
-require('dotenv').config();
-const express = require('express');
-const { createBusAccessTable } = require('./db/createTable');
-const { sendAccess } = require('./producer/sendBusAccessKafka');
-const { initKafkaProducer } = require('./config/kafka');
-const app = express();
+require("dotenv").config();
+const cluster = require("cluster");
+const os = require("os");
+const express = require("express");
+const { createBusAccessTable } = require("./db/createTable");
+const { sendAccess } = require("./producer/sendBusAccessKafka");
+const { initKafkaProducer } = require("./config/kafka");
 
-app.use(express.json());
-
-createBusAccessTable()
-  .then(() => console.log('bus_access table ensured'))
-  .catch((err) => console.error('Error creating table:', err));
-
-app.post('/app/sendAccess', sendAccess);
-
+const numCPUs = os.cpus().length;
 const PORT = process.env.PORT || 3000;
-initKafkaProducer()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log('Server listening on port 3000...');
-    });
-  })
-  .catch(err => {
-    console.error('Error connecting Kafka Producer:', err);
-    process.exit(1);
+
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
   });
+} else {
+  console.log(`Worker ${process.pid} started`);
+
+  const app = express();
+  app.use(express.json());
+
+  createBusAccessTable()
+    .then(() => console.log("bus_access table ensured"))
+    .catch((err) => console.error("Error creating table:", err));
+
+  initKafkaProducer()
+    .then(() => {
+      app.post("/app/sendAccess", sendAccess);
+      app.listen(PORT, () => {
+        console.log(`Worker ${process.pid} listening on port ${PORT}...`);
+      });
+    })
+    .catch((err) => {
+      console.error("Error connecting Kafka Producer:", err);
+      process.exit(1);
+    });
+}
